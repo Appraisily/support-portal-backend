0925afe46ad65b39f340ff8c85a75bee1f69b5ab
Appraisily <134934621+Appraisily@users.noreply.github.com>
2024-11-04 11:26:51 +0100

    Update dbTest.js

--- src/utils/dbTest.js
+++ src/utils/dbTest.js
@@ -1,10 +1,8 @@
 const { Client } = require('pg');
-const fs = require('fs');
 const logger = require('./logger');
 
 async function testDatabaseConnection() {
   const {
-    CLOUD_SQL_CONNECTION_NAME,
     DB_NAME,
     DB_USER,
     DB_PASSWORD
@@ -15,44 +13,17 @@ async function testDatabaseConnection() {
   // 1. Check environment variables
   logger.info('1. Checking environment variables...');
   const envCheck = {
-    CLOUD_SQL_CONNECTION_NAME: !!CLOUD_SQL_CONNECTION_NAME,
     DB_NAME: !!DB_NAME,
     DB_USER: !!DB_USER,
     DB_PASSWORD: !!DB_PASSWORD
   };
   logger.info('Environment variables present:', envCheck);
 
-  // 2. Check Unix socket directory
-  const socketPath = `/cloudsql/${CLOUD_SQL_CONNECTION_NAME}`;
-  logger.info('2. Checking Unix socket path:', socketPath);
-  
-  try {
-    const socketExists = fs.existsSync(socketPath);
-    logger.info('Socket path exists:', socketExists);
-    
-    if (socketExists) {
-      const stats = fs.statSync(socketPath);
-      logger.info('Socket file permissions:', {
-        mode: stats.mode.toString(8),
-        uid: stats.uid,
-        gid: stats.gid
-      });
-
-      const contents = fs.readdirSync('/cloudsql');
-      logger.info('Contents of /cloudsql directory:', contents);
-    }
-  } catch (error) {
-    logger.error('Error checking socket:', {
-      error: error.message,
-      code: error.code,
-      path: error.path
-    });
-  }
-
-  // 3. Test PostgreSQL connection
-  logger.info('3. Testing PostgreSQL connection...');
+  // 2. Test PostgreSQL connection
+  logger.info('2. Testing PostgreSQL connection...');
   const client = new Client({
-    host: socketPath,
+    host: '34.57.184.164',
+    port: 5432,
     database: DB_NAME,
     user: DB_USER,
     password: DB_PASSWORD,
@@ -73,11 +44,10 @@ async function testDatabaseConnection() {
     return true;
   } catch (error) {
     logger.error('Database connection test failed:', {
-      message: error.message,
       code: error.code,
+      message: error.message,
       detail: error.detail,
-      hint: error.hint,
-      position: error.position
+      hint: error.hint
     });
     return false;
   }
