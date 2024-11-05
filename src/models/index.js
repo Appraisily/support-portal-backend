b7dd9ae056fa2b235ec6d77da5c5ad7728dbbe86
Appraisily <134934621+Appraisily@users.noreply.github.com>
2024-11-05 13:15:03 +0100

    sa

--- src/models/index.js
+++ src/models/index.js
@@ -1,19 +1,46 @@
-const mongoose = require('mongoose');
+const { Sequelize } = require('sequelize');
 const logger = require('../utils/logger');
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    logger.info('Connected to MongoDB');
-  } catch (error) {
-    logger.error('MongoDB connection error:', error);
-    process.exit(1);
-  }
+let sequelize;
+
+if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: (msg) => logger.debug(msg)
+  });
+} else {
+  // Configuración para desarrollo/testing
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: ':memory:',
+    logging: false
+  });
+}
+
+const models = {
+  Customer: require('./customer')(sequelize),
+  Ticket: require('./ticket')(sequelize),
+  Message: require('./message')(sequelize),
+  Setting: require('./setting')(sequelize)
 };
 
+// Configurar relaciones
+Object.keys(models).forEach(modelName => {
+  if (models[modelName].associate) {
+    models[modelName].associate(models);
+  }
+});
+
+// Función para obtener los modelos (asegura que la conexión está lista)
+async function getModels() {
+  if (!sequelize) {
+    throw new Error('Database not initialized');
+  }
+  return models;
+}
+
 module.exports = {
-  connectDB,
-  Ticket: require('./ticket'),
-  Message: require('./message'),
-  Attachment: require('./attachment')
+  sequelize,
+  getModels
 };
